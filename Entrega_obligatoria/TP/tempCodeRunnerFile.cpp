35bc01f778fbc0742ce93da51e4fdf7d47b883c0
Mayra Melina Rossi <mayra.rossi03@gmail.com>
2021-11-08 18:27:25 -0300

    ok

---
+++ Entrega_obligatoria/TP/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+    // Arch_rep r;
+    // fread(&r, sizeof(Arch_rep), 1, f);
+    // while(!feof(f) && r.zona != zona)
+    // {
+    //     fread(&r, sizeof(Arch_rep), 1, f);
+    // }
+    // if(feof(f))
+    //     return false;
+    // else
