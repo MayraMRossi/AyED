cf8cc86f221ad26ee323c3d2553b6073840767b4
Mayra Melina Rossi <81454006+MayraMRossi@users.noreply.github.com>
2021-11-30 22:56:09 -0300

    ok

--- Modelos parcial/recuinfiltrado/prueba_de_fuego.cpp
+++ Modelos parcial/recuinfiltrado/prueba_de_fuego.cpp
@@ -7,50 +7,69 @@ using namespace std;
 struct Estudiantes
 {
     unsigned legajo;
-    char apellido_nombre[20];
+    string nombre;
     unsigned Cod_beca;
     float Ingreso_prom;
-};
+}
 
 struct Becas
 {
     unsigned Cod_beca;
     string Nombre;
     unsigned Cupo_max;
-};
+    Nodo_estudiantes *lista_estudiantes;
+}
 
 struct Nodo_becas
 {
     Becas info;
-    Nodo_becas *sig;
-};
+    Nodo_becas sig;
+}
 
 struct Nodo_estudiantes
 {
     Estudiantes info;
-    Nodo_estudiantes *sig;
-};
+    Nodo_estudiantes sig;
+}
+
+Nodo_becas ingresar_becas ();
+Nodo_estudiantes organizarInscriptos();
+void mostrarListado (Nodo_becas *listaBec,Nodo_estudiantes *listaEst);
 
 
-Nodo_becas *ingresar_becas ()
+
+int main{
+
+
+
+    Nodo_becas lis1 = ingresar_becas();
+    Nodo_estudiantes lis2 = organizarInscriptos();
+    mostrarListado (lis1,lis2);
+    return 0;
+}
+
+
+
+
+Nodo_becas ingresar_becas ()
 {
-    Becas b;
+    Estudiantes alu;
     Nodo_becas *listaBec = NULL;
 
     cout<<"Ingrese el codigo de la beca: (0 para salir)"<<endl;
-    cin >> b.Cod_beca >> endl;
+    cin >> alu.Cod_beca >> endl;
 
-    while(b.Cod_beca!= 0)
+    while(alu.Cod_beca!= 0)
     {
         cout << "Ingrese el nombre de la beca: " << endl;
-        cin >> b.nombre >> endl;
+        cin >> alu.nombre >> endl;
         cout << "Ingrese el cupo maximo: " << endl;
-        cin >> b.Cupo_max >> endl;
+        cin >> alu.Cupo_max >> endl;
 
-        insertar(listaBec,b); //función básica "insertar" ordenado de menor a mayor según el código de beca con la lista pasada como parametro por referencia
+        insertar(listaBec,alu); //función básica "insertar" ordenado de menor a mayor según el código de beca con la lista pasada como parametro por referencia
 
         cout<<"Ingrese el codigo de la beca: (0 para salir)"<<endl;
-        cin >> b.Cod_beca >> endl;
+        cin >> alu.Cod_beca >> endl;
     }
 
     return listaBec;
@@ -59,24 +78,34 @@ Nodo_becas *ingresar_becas ()
 
 
 
-Nodo_estudiantes *organizarInscriptos()
+Nodo_estudiantes organizarInscriptos()
 {
+    Estudiantes e [10];
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Legajo: "<<endl;
+        cin>> e.legajo >> endl;
+        cout << "Nombre: "<<endl;
+        cin>> e.nombre >> endl;
+        cout << "cod: "<<endl;
+        cin>> e.Cod_beca >> endl;
+        cout << "Ingreso prom: "<<endl;
+        cin>> e.ingreso_prom >> endl;
+
+            Nodo_estudiantes *listaEst = NULL;
     
-    Estudiantes est;
-    Nodo_estudiantes *listaEst = NULL;
-    //Cargo el archivo en una lista simple
-    FILE*f = fopen("Solicitantes.dat", "rb");
-    if(f != NULL)
-    {      
-        fread(&est, sizeof(Estudiantes), 1, f);  
-        while(!feof(f))
-        {
+     
+
             
             insertar(listaEst, est); //función basica "insertar" ordenado de menor a mayor según el código de beca con la lista pasada como parametro por referencia
-            fread(&est, sizeof(Estudiantes), 1, f);
-        }
-        fclose(f);
+            
+        
     }
+
+    
+
+    
+    
 
     return listaEst;
 }
@@ -90,7 +119,7 @@ void mostrarListado (Nodo_becas *listaBec,Nodo_estudiantes *listaEst)
     while (listaBec != NULL)
     {   
         int cont=0;
-        cout << "Para la beca de código " << listaBec->info->Cod_beca << " y nombre " << listaBec->info->Nombre << " tiene asignados los siguientes alumnos:  "
+        Cout << "Para la beca de código " << listaBec->info->Cod_beca << " y nombre " << listaBec->info->Nombre << " tiene asignados los siguientes alumnos:  "
         
         while(listaEst !=NULL && listaEst->info->Cod_beca == listaBec->info->Cod_beca)
         {
